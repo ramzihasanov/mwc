1b9bad7da8ef9323e31444b0b8d335010122c398
ramzihasanov <bd54tkjwr@code.edu.az>
2023-11-15 12:09:54 +0400

    mvc

---
+++ Mwc/Controllers/BB206Controller.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC.Controllers
+{
+    public class BB206Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public JsonResult JsonResult()
+        {
+            return new JsonResult("age : 19 ");
+        }
+
+
+        public ContentResult Content()
+        {
+            return Content("Hello World");
+        }
+
+        
+
+        
+
+
+       
+    }
+}
